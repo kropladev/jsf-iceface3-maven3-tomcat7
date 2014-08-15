d440b4d74220963fba78e2cfd629b88e267a5748
kropla <makroplewski@gmail.com>
2014-08-16 00:08:54 +0200

    initial codes

---
+++ src/main/java/dev/kropla/tutorial/HelloWorld.java
@@ -0,0 +1,26 @@
+package dev.kropla.tutorial;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class HelloWorld implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String name;
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+}
